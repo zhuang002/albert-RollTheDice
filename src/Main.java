4f38e3b74aa79467a07a9c88b75d540c9684fadc
zhuang002 <zhuang001@gmail.com>
2022-11-20 19:56:46 -0500

    correct version

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int count = 0;
+		for (int i=1;i<=m;i++) {
+			if (i>10) 
+				break;
+			if (10-i<=n) {
+				count++;
+			}
+			
+		}
+		if (count == 1) {
+			System.out.println("There is 1 way to get the sum 10.");
+		} else {
+			System.out.println("There are "+count+" ways to get the sum 10.");
+		}
+	}
+
+}
